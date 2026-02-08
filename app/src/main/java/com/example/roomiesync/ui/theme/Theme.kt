ed45da9fd8ef78ffa0058741e8082f712b1b2496
eh128 <ethanhsu128@gmail.com>
2026-02-08 12:49:08 -0500

    Define colors for app

--- app/src/main/java/com/example/roomiesync/ui/theme/Theme.kt
+++ app/src/main/java/com/example/roomiesync/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimaryGreen,
+    secondary = DarkSecondaryGrey,
+    tertiary = DarkTertiaryGreen
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryGreen,
+    secondary = SecondaryGrey,
+    tertiary = TertiaryGreen
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 fun RoomieSyncTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
